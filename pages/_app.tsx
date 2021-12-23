4c3a7064a6e8d79cf3e99bef9d8a0fa85b6c83e7
Angelo Silvestre <angelosilvestre.ccp@gmail.com>
2021-12-22 21:31:06 -0300

    Remove unused dependency on react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,20 +1,13 @@
 import type { AppProps } from 'next/app';
 import React from 'react';
-import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
 import Layout from '../components/Layout';
 import 'semantic-ui-css/semantic.min.css';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [queryClient] = React.useState(() => new QueryClient());
-
   return (
-    <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </Hydrate>
-    </QueryClientProvider>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
   );
 }
 
